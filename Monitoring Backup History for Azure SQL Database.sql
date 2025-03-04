db152f39e218abbecf20fa95d42b9681e5ef614e
Subbu CN <111134712+AzureSQLChamp@users.noreply.github.com>
2025-03-04 17:54:33 +0530

    Update Monitoring Backup History for Azure SQL Database.sql

--- Monitoring Backup History for Azure SQL Database.sql
+++ Monitoring Backup History for Azure SQL Database.sql
@@ -1,3 +1,29 @@
+/*
+Backup frequency
+Azure SQL Managed Instance creates:
+
+Full backups every week.
+Differential backups every 12 to 24 hours.
+Transaction log backups every 5-10 minutes.
+The frequency of transaction log backups is based on the compute size and the amount of database activity. When you restore a database, the service determines which full, differential, and transaction log backups need to be restored.
+
+ 
+
+How to monitor Azure SQL Database History backups
+Azure SQL Database Backup History introduced a new Dynamic Management View(DMV) called Sys.dm_database_backups, that contains metadata information on all the active backups that are needed for enabling point-in-time restore within configured retention. 
+Metadata information includes:
+
+Backup_file_id – Backup file ID
+Database_guid –Logical Database ID
+Physical_Database_name – Physical Database Name
+Server_name – Physical Server Name
+Backup_start_date – Backup Start Timestamp
+Backup_finish_date – Backup End Timestamp
+Backup_Type – Type of Backup. D stands for Full Database Backup, L – Stands for Log Backup and I – Stands for differential backup
+In_Retention – Whether backup is within retention period or not. 1 stands for within retention period and 0 stands for out of retention
+*/
+
+
 #SCRIPT-1
 =======
 SELECT db.name
